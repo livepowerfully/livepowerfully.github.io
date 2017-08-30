53d5f31276b61e3caf7a7907957639b8b911ce2e
Mandar Gogate <mandargogate@users.noreply.github.com>
2017-08-30 09:15:42 +0100

    pre

---
+++ README.md.bak
@@ -0,0 +1,1 @@
+# livepowerfully.github.io
--- preprocess.sh
+++ preprocess.sh
@@ -1,1 +1,2 @@
-find . -type f -print0 | xargs -0 perl -i.bak -pe 's/livepowerfully.github.io:8080\/wordpress//g'
+#find . -type f -print0 | xargs -0 perl -i.bak -pe 's/livepowerfully.github.io:8080\/wordpress//g'
+find . -type f -print0 | xargs -0 perl -i.bak -pe 's/\//g'
